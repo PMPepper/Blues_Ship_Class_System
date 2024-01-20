dbf63e216f76861d52b21c6057e9c4d0e8140c3e
DESKTOP-2D91HMA\Paul <pm_pepper@hotmail.com>
2024-01-20 09:25:20 +0000

    Refactoring

--- Data/Scripts/Blues_Ship_Matrix/GridMatrix_Manager.cs
+++ Data/Scripts/Blues_Ship_Matrix/GridMatrix_Manager.cs
@@ -33,7 +33,7 @@ namespace Blues_Ship_Matrix
 	[MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
 	public class Manager : MySessionComponentBase
 	{
-
+		public const int MIN_BLOCKCOUNT_THRESHOLD = 4;//grids with fewer blocks than this will be ignored
 		public static List<IMyCubeGrid> GridList;
 		public static bool IsClient => !(IsServer && IsDedicated);
 		public static bool IsDedicated => MyAPIGateway.Utilities.IsDedicated;
@@ -52,6 +52,7 @@ namespace Blues_Ship_Matrix
 
 			if (IsServer)
 			{
+				//Initialise settings
 				MySettings = ShipMatrixConfig.Load();
 				ShipMatrixConfig.Save(MySettings);
 				MyLog.Default.WriteLine("Blues_Ship_Matrix: Server Loaded and Saved Config");
@@ -66,135 +67,144 @@ namespace Blues_Ship_Matrix
 
 		public override void UpdateAfterSimulation()
 		{
-			base.UpdateBeforeSimulation();//this seems strange?
-			if (MySettingsSynced.Value != null && !IsDedicated) { MySettings = MySettingsSynced.Value; }
+			base.UpdateAfterSimulation();
+
+			//Not currently 100% clear what this does, still working through the code
+			if (MySettingsSynced.Value != null && !IsDedicated)
+			{
+				MySettings = MySettingsSynced.Value;
+			}
+
 			ticks += 1;
+
 			MyAPIGateway.Parallel.Start(delegate
 			{
 				if (IsServer)
 				{
 					//Only Send Config If you are the server
-					if (ticks > 240) { ticks = 0; MySettingsSynced.ValidateAndSet(MySettings); }
-
-					foreach (IMyCubeGrid CoreGrid in GridList.ToList())
+					if (ticks > 240)
 					{
-						if ((CoreGrid as MyCubeGrid).BlocksCount < 4) { continue; }
-						IEnumerable<IMyBeacon> Beacons = CoreGrid.GetFatBlocks<IMyBeacon>();
-						List<IMyBeacon> BeaconsList = Beacons.ToList();
-						long GridOwner = GetOwner(CoreGrid as MyCubeGrid);
-						var OwnerFaction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(GridOwner);
-						if (OwnerFaction != null)
+						ticks = 0;//this does not seem thread safe?
+						MySettingsSynced.ValidateAndSet(MySettings);
+					}
+
+					foreach (IMyCubeGrid CoreGrid in GridList)
+					{
+						long GridOwner;
+
+						if (IsIgnoredGrid(CoreGrid, out GridOwner))
 						{
-							if (OwnerFaction.Tag != null || OwnerFaction.Tag != "")
-							{
-								if (MySettings.IgnoredFactions.Contains(OwnerFaction.Tag)) { continue; }
-							}
-						}
-						if (BeaconsList.Count < 1)
-						{
-							IEnumerable<IMyFunctionalBlock> BlocksOnGrid = CoreGrid.GetFatBlocks<IMyFunctionalBlock>();
-							foreach (IMyFunctionalBlock Block in BlocksOnGrid.ToList())
-							{
-								if (Block == null) { continue; }
-								if (Block.Enabled) { Block.Enabled = false; }
-							}
 							continue;
 						}
-						else
+
+
+						var Beacons = CoreGrid.GetFatBlocks<IMyBeacon>();
+
+						if (Beacons.Count() < 1)
 						{
-							//MySync<string, SyncDirection.BothWays> SyncedWarning;
-							string Warning = "";
-							bool Penalize = false;
-							IMyBeacon CoreBeacon = BeaconsList.First();
-							if (CoreBeacon == null) { continue; }
-							var MyShipCore = CoreBeacon?.GameLogic?.GetAs<ShipCore>();
-							MyGridLimit CoreGridClass = MyShipCore.CoreGridClass;
-							if (MyShipCore.CoreGridClass == null)
-							{
-								CoreGridClass = Globals.GetClass(CoreGrid);
-								MyShipCore.CoreGridClass = CoreGridClass;
-							}
-							/*if(CoreGridClass==null||(!CoreGrid.CustomName.ToLower().Contains(CoreGridClass.Name.ToLower()))||CoreGrid.CustomName!=CoreBeacon.CustomData)
-							{
-								CoreGridClass = Globals.GetClass(CoreGrid));
-								SyncWarning.ValidateAndSet(SyncWarning.Value+=ClassLimit.Warning);
-								CoreBeacon.CustomData=CoreGridClass.Name;
-							}*/
-							Warning += "\n<<< Class Limits Info: >>>\nClass Name:" + CoreGridClass.Name;
-							string PerStatWarning = "";
-							string PerBlockWarning = "";
-							if (ticks % 3 == 0)
-							{
-								try
-								{
-									PerBlockWarning += Globals.CheckAndPenalizeBlockLimits(CoreBeacon, CoreGridClass);
-								}
-								catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @BlockLimits - {e.Message}"); }
-							}
-							if (ticks % 6 == 0)
-							{
-								try
-								{
-									DoubleReturn GridStatLimit = Globals.CheckGridStatLimits(CoreBeacon, CoreGridClass);
-									if (GridStatLimit.Penalty) { Penalize = GridStatLimit.Penalty; }
-									PerStatWarning += GridStatLimit.Warning;
-								}
-								catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @GridStats - {e.Message}"); }
-							}
-							if (ticks % 12 == 0)
-							{
-								//MySettingsSynced.ValidateAndSet(MySettings);
-								try
-								{
-									DoubleReturn ClassLimit = Globals.CheckClassLimits(CoreBeacon, CoreGridClass);
-									if (ClassLimit.Penalty) { Penalize = ClassLimit.Penalty; }
-									Warning += ClassLimit.Warning + PerStatWarning + PerBlockWarning + MyShipCore.Info;
-								}
-								catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @ClassLimits - {e.Message}"); }
-								//MyShipCore.CoreBeacon.CustomData=Warning;
-								//MyShipCore.Warning=Warning;
-								foreach (IMyBeacon Beacon in BeaconsList.ToList())
-								{
-									if (Beacon == null) { continue; }
-									if (Beacon?.GameLogic?.GetAs<ShipCore>() != null)
-									{
-										var AuxShipCore = Beacon?.GameLogic?.GetAs<ShipCore>();
-										if (CoreBeacon != Beacon) { AuxShipCore.Warning = "Warning: Not Primary Beacon\n" + Warning; } else { AuxShipCore.Warning = Warning; }
-										if (GridOwner != Beacon.OwnerId) { AuxShipCore.Warning = "Warning: Beacon Owner Does NOT Own Grid\n" + Warning; }
-									}
+							PenaliseInvalidGrid(CoreGrid);
 
-								}
-								//if(MyShipCore!=null){MyShipCore.CustomData=Warning;}
-								//CoreBeacon.RefreshCustomInfo();
-								//SyncedWarning.ValidateAndSet(Warning);
+							continue;
+						}
 
-							}
-							//CoreBeacon.CustomData=Warning;
-							/*Dictionary<long, string> messages = new Dictionary<long, string>();
-							if(SyncMessages.Value.MSG!=null)
-							{
-									messages = SyncMessages.Value.MSG;
-							}
-							messages[CoreBeacon.EntityId] = Warning;
-							SyncMessages.ValidateAndSet(new MessageStorage{MSG=messages});
-							*/
-							//Penalize offenders
+						//MySync<string, SyncDirection.BothWays> SyncedWarning;
+						string Warning = "";
+						bool Penalize = false;
+						IMyBeacon CoreBeacon = Beacons.First();
+
+						if (CoreBeacon == null)
+						{
+							MyLog.Default.WriteLine("Blues_Ship_Matrix: SHOULD NOT HAPPEN: First beacon in list is null");
+							continue;
+						}
+
+						var MyShipCore = CoreBeacon?.GameLogic?.GetAs<ShipCore>();
+						MyGridLimit CoreGridClass = MyShipCore.CoreGridClass;
+						if (MyShipCore.CoreGridClass == null)
+						{
+							CoreGridClass = Globals.GetClass(CoreGrid);
+							MyShipCore.CoreGridClass = CoreGridClass;
+						}
+						/*if(CoreGridClass==null||(!CoreGrid.CustomName.ToLower().Contains(CoreGridClass.Name.ToLower()))||CoreGrid.CustomName!=CoreBeacon.CustomData)
+						{
+							CoreGridClass = Globals.GetClass(CoreGrid));
+							SyncWarning.ValidateAndSet(SyncWarning.Value+=ClassLimit.Warning);
+							CoreBeacon.CustomData=CoreGridClass.Name;
+						}*/
+						Warning += "\n<<< Class Limits Info: >>>\nClass Name:" + CoreGridClass.Name;
+						string PerStatWarning = "";
+						string PerBlockWarning = "";
+						if (ticks % 3 == 0)
+						{
 							try
 							{
-								if (Penalize) { Globals.Penalize(CoreBeacon); }
+								PerBlockWarning += Globals.CheckAndPenalizeBlockLimits(CoreBeacon, CoreGridClass);
 							}
-							catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @Penalize - {e.Message}"); }
-							/*if(IsDedicated)
-							{
-								try{
-									if(ticks % 1000 == 0)
-									{
-										//Globals.Messages[CoreBeacon.EntityId]=CoreBeacon.CustomData;
-										CoreBeacon.RefreshCustomInfo();
-									}
-								catch (Exception e){MyLog.Default.WriteLine($"BlueShipMatrix: Error @Client - {e.Message}");}
-							}*/
+							catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @BlockLimits - {e.Message}"); }
 						}
+						if (ticks % 6 == 0)
+						{
+							try
+							{
+								DoubleReturn GridStatLimit = Globals.CheckGridStatLimits(CoreBeacon, CoreGridClass);
+								if (GridStatLimit.Penalty) { Penalize = GridStatLimit.Penalty; }
+								PerStatWarning += GridStatLimit.Warning;
+							}
+							catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @GridStats - {e.Message}"); }
+						}
+						if (ticks % 12 == 0)
+						{
+							//MySettingsSynced.ValidateAndSet(MySettings);
+							try
+							{
+								DoubleReturn ClassLimit = Globals.CheckClassLimits(CoreBeacon, CoreGridClass);
+								if (ClassLimit.Penalty) { Penalize = ClassLimit.Penalty; }
+								Warning += ClassLimit.Warning + PerStatWarning + PerBlockWarning + MyShipCore.Info;
+							}
+							catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @ClassLimits - {e.Message}"); }
+							//MyShipCore.CoreBeacon.CustomData=Warning;
+							//MyShipCore.Warning=Warning;
+							foreach (IMyBeacon Beacon in Beacons)
+							{
+								if (Beacon == null) { continue; }
+								if (Beacon?.GameLogic?.GetAs<ShipCore>() != null)
+								{
+									var AuxShipCore = Beacon?.GameLogic?.GetAs<ShipCore>();
+									if (CoreBeacon != Beacon) { AuxShipCore.Warning = "Warning: Not Primary Beacon\n" + Warning; } else { AuxShipCore.Warning = Warning; }
+									if (GridOwner != Beacon.OwnerId) { AuxShipCore.Warning = "Warning: Beacon Owner Does NOT Own Grid\n" + Warning; }
+								}
+
+							}
+							//if(MyShipCore!=null){MyShipCore.CustomData=Warning;}
+							//CoreBeacon.RefreshCustomInfo();
+							//SyncedWarning.ValidateAndSet(Warning);
+
+						}
+						//CoreBeacon.CustomData=Warning;
+						/*Dictionary<long, string> messages = new Dictionary<long, string>();
+						if(SyncMessages.Value.MSG!=null)
+						{
+								messages = SyncMessages.Value.MSG;
+						}
+						messages[CoreBeacon.EntityId] = Warning;
+						SyncMessages.ValidateAndSet(new MessageStorage{MSG=messages});
+						*/
+						//Penalize offenders
+						try
+						{
+							if (Penalize) { Globals.Penalize(CoreBeacon); }
+						}
+						catch (Exception e) { MyLog.Default.WriteLine($"BlueShipMatrix: Error @Penalize - {e.Message}"); }
+						/*if(IsDedicated)
+						{
+							try{
+								if(ticks % 1000 == 0)
+								{
+									//Globals.Messages[CoreBeacon.EntityId]=CoreBeacon.CustomData;
+									CoreBeacon.RefreshCustomInfo();
+								}
+							catch (Exception e){MyLog.Default.WriteLine($"BlueShipMatrix: Error @Client - {e.Message}");}
+						}*/
 					}
 				}
 				/*if(IsDedicated)
@@ -252,6 +262,44 @@ namespace Blues_Ship_Matrix
 				return gridOwnerList[1];
 
 			return gridOwner;
+		}
+
+		public bool IsIgnoredGrid(IMyCubeGrid Grid, out long GridOwner)
+		{
+			GridOwner = 0L;
+
+			if ((Grid as MyCubeGrid).BlocksCount < MIN_BLOCKCOUNT_THRESHOLD)
+			{
+				MyAPIGateway.Utilities.ShowMessage("Blues_Ship_Matrix: ignore grid, size = ", Convert.ToString((Grid as MyCubeGrid).BlocksCount));
+				return true;
+			}
+
+			GridOwner = GetOwner(Grid as MyCubeGrid);
+			var OwnerFaction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(GridOwner);
+
+			if (OwnerFaction != null && !String.IsNullOrEmpty(OwnerFaction.Tag))
+			{
+				// if (MySettings.IgnoredFactions.Contains(OwnerFaction.Tag))
+				// {
+				// 	MyAPIGateway.Utilities.ShowMessage("Blues_Ship_Matrix: ignore grid, faction = ", OwnerFaction.Tag);
+				// }
+
+				return MySettings.IgnoredFactions.Contains(OwnerFaction.Tag);
+			}
+
+			return false;
+		}
+
+		private void PenaliseInvalidGrid(IMyCubeGrid Grid)
+		{
+			//Currently just turns off all functional blocks
+			foreach (IMyFunctionalBlock Block in Grid.GetFatBlocks<IMyFunctionalBlock>())
+			{
+				if (Block != null && Block.Enabled)
+				{
+					Block.Enabled = false;
+				}
+			}
 		}
 
 		private void InitGridTracking()
